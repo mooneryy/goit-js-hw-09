3081c1b2b21e7e237845ee44ccdb9e19d9fdae65
mooneryy <yelisyeyevaa@gmail.com>
2024-02-09 02:10:52 +0200

    add

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,7 @@
-console.log("Gallery");
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+console.log(SimpleLightbox);
