7539e27b5648f7c39b6e5d2b5a973e291aa807af
mooneryy <145297415+mooneryy@users.noreply.github.com>
2024-02-09 00:11:19 +0000

    Deploying to gh-pages from @ mooneryy/goit-js-hw-09@3081c1b2b21e7e237845ee44ccdb9e19d9fdae65 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/styles-3c4e11fa.js";console.log("Gallery");
+import"./assets/styles-3c4e11fa.js";import{S as o}from"./assets/vendor-870f0eb5.js";new o(".gallery a",{});console.log(o);
 //# sourceMappingURL=commonHelpers.js.map
